f1c2df2e7d109e52bd794c2a1f161dad3f57b73a
amartin3zp <almapa@fp.insjoaquimmir.cat>
2023-09-22 15:20:26 +0200

    actualicacion del fitchero gitignore

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask ,render_template ,g
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('hello.html')
+
+@app.route("/products")
+def hello_world():
+    return render_template('hello.html')
+
+DATABASE = '/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
