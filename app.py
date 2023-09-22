32de7f549d844be232b51b892bedda39799195db
amartin3zp <almapa@fp.insjoaquimmir.cat>
2023-09-22 17:33:32 +0200

    conexion con base de datos

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask ,render_template ,g
+from flask import Flask ,render_template ,g ,request
 import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'database.db'
+
+
+def get_db():
+    # sqlite3_database_path = DATABASE
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route("/")
 def hello_world():
     return render_template('hello.html')
 
-@app.route("/products")
-def hello_world():
-    return render_template('hello.html')
-
-DATABASE = '/database.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+@app.route("/list")
+def list():
+    with get_db() as conn:
+        results = conn.execute("SELECT * FROM products ORDER BY id ASC")
+        items = results.fetchall()
+    return render_template("products/list.html",items = items)
